fcb0b027cdff55dee1fb118a1944cebb74b2d2bc
denervalspelizario <denervalspelizario@gmail.com>
2023-05-01 11:26:04 -0300

    update sitePessoal adicao de comentarios nos componets header e activelink

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,19 +1,24 @@
 import { ReactElement, cloneElement } from "react";
-import Link, {LinkProps} from "next/link";
-import { useRouter } from "next/router";
+import Link, {LinkProps} from "next/link";  
+import { useRouter } from "next/router"; // pegando a biblioteca de rotas
 
-interface  ActiveLinkProps extends LinkProps {
 
-  children: ReactElement;
-  activeClassName: string;
+// LinkProps é uma interface para personalizar um link de roteamento dentro do Next.js.
+
+interface  ActiveLinkProps extends LinkProps {      
+
+  children: ReactElement; // children será um elemento react( toda estrutura de react ) 
+  activeClassName: string;  // className sendo tipado como string
 
 }
 
-export function ActiveLink({ children, activeClassName, ...rest }: ActiveLinkProps){
+export function ActiveLink({ children, activeClassName, ...rest }: ActiveLinkProps){ // activeLink recebendo interface ActiveLinkProps + LinkProps
 
-  const { asPath } = useRouter();
+  const { asPath } = useRouter(); //  extraindo a rota atual da página usando a função useRouter() e armazenando-a na variável asPath.
+                                  // ou seja asPath é uma propriedade que representa a parte da URL que vem após o nome do domínio. 
 
-  const className = asPath === rest.href ? activeClassName : '';
+  const className = asPath === rest.href ? activeClassName : ''; // se asPath(rota da url) for igual a resthref( rota atual(pagina que se está)) então 
+                                                                 // chama a activeclassname(ela fica true) que ativa la no style.module  senão não faz nada  
 
   return(
     <Link {...rest}>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'; // importando Image que trata imagens no next
 import Logo from '../../../public/images/logo.svg'
 import styles from './style.module.scss'
 
-import { ActiveLink } from '../ActiveLink';
+import { ActiveLink } from '../ActiveLink'; // importando o component activeLink que será responsável pelas rotas e de ativar os link quando pagina estiver ativa
 
 
 
@@ -12,7 +12,9 @@ export  function Header() {
     <header className={styles.headerContainer}>
       <div className={styles.headerConteudo}>
 
-        <ActiveLink href="/" activeClassName={styles.active} >
+        <ActiveLink href="/" 
+          activeClassName={styles.active} // linkando a estilização ao active, os elementos dentro do paragrafos são estilizados normalmente dentro de style.module 
+        >
           <p>
             <Image src={Logo} alt='Programador Logo' />
           </p>
@@ -32,6 +34,7 @@ export  function Header() {
           </ActiveLink>
         </nav>
 
+        
         <a type='button' className={styles.btnHeader}  href="">Começar</a>
 
       </div>
