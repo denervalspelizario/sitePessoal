223a5a7e09b39555bcd897f4cf68ae773545ad34
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-10-15 15:14:54 -0300

    feat: criacao de html e estilizacao da page sites-institucionais

---
+++ src/app/modules/portifolio/interface/IProjetosAngInterface.ts
@@ -0,0 +1,8 @@
+
+export interface IProjetosAng {
+  title: string;
+  description: string;
+  src: string;
+  alt: string;
+  route: string;
+}
--- src/app/modules/portifolio/pages/sites-intitucionais/sites-intitucionais.component.ts
+++ src/app/modules/portifolio/pages/sites-intitucionais/sites-intitucionais.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IProjetosAng } from '../../interface/IProjetosAngInterface';
 
 @Component({
   selector: 'app-sites-intitucionais',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './sites-intitucionais.component.scss'
 })
 export class SitesIntitucionaisComponent {
+
+  public arrayProjetos = signal<IProjetosAng[]>([
+    {
+      src: '../../../../../assets/baiao-rockbar/baiao-logo.png',
+      alt: 'logo do site baiao-rock-bar',
+      description: 'Sinta a energia do rock no Baião, o melhor rock bar de João Pessoa! 🤘',
+      title: 'Baião Rock Bar',
+      route: '#'
+    },
+
+
+
+  ]);
+
 
 }
